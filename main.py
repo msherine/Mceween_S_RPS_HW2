97c87041e3f1df931b8fa82b7eea30fa5016d9da
msherine <sherine121098@gmail.com>
2021-11-08 22:56:38 -0500

    loop working

--- main.py
+++ main.py
@@ -3,43 +3,59 @@ from random import randint
 choices = ["rock", "paper", "scissors"]
 
 # player will be the weapon and the player will choose via input
-player = input("Choose your weapon: rock, paper or scissors: ")
-
-computer = choices[randint(0,2)]
+player = False
 
 # these lives need to decrement when a player chooses via input 
 playerLives = 5
 computerLives = 5
 
-print("player choose" = player)
-print("computer choose: " + computer)
+while player is False:
 
-if (computer == player):
-	print("Tie! Try again.")
+	player = input("Choose your weapon: rock, paper or scissors: ")
+	computer = choices[randint(0, 2)]
 
-elif (player == "rock"):
-	if (computer == "paper"):
-		print("You lose!")
-		playerLives = playerLives - 1
-	else:
-		print("You win!")
-		computerLives = computerLives - 1
+	print("player choose: " + player)
+	print("computer choose: " + computer)
 
-elif (player == "paper"):
-	if (computer == "scissors"):
-		print("You lose!")
-		playerLives = playerLives - 1
-	else:
-		print("You win!")
-		computerLives = computerLives - 1
+	if (computer == player):
+		print("Tie! Try again.")
 
-elif (player == "scissors"):
-	if (computer == "rock"):
-		print("You lose!")
-		playerLives = playerLives - 1
-	else:
-		print("You win!")
-		computerLives = computerLives - 1
+	elif (player == "rock"):
+		if (computer == "paper"):
+			print("You lose!")
+			playerLives = playerLives - 1
+		else:
+			print("You win!")
+			computerLives = computerLives - 1
 
-print("computer lives: " + str(computerLives))
-print("player lives: " + str(playerLives))
+	elif (player == "paper"):
+		if (computer == "scissors"):
+			print("You lose!")
+			playerLives = playerLives - 1
+		else:
+			print("You win!")
+			computerLives = computerLives - 1
+
+	elif (player == "scissors"):
+		if (computer == "rock"):
+			print("You lose!")
+			playerLives = playerLives - 1
+		else:
+			print("You win!")
+			computerLives = computerLives - 1
+
+	print("computer lives: " + str(computerLives))
+	print("player lives: " + str(playerLives))
+
+	if playerLives == 0:
+		print("You lost! Would you like to play again?")
+		choice = input(" Y / N? ")
+
+		if choice == "n":
+			print(" Better luck next time!")
+			exit()
+			
+	elif computerLives == 0:
+		print(" You win!!")
+
+	player = False
