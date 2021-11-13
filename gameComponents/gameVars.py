e57ea4abf83d28c6afb7b1cb24d2ce020a577bad
msherine <sherine121098@gmail.com>
2021-11-12 20:15:20 -0500

    working

---
+++ gameComponents/gameVars.py
@@ -0,0 +1,6 @@
+# set up a variable, check its value, and chose a path to follow based on that condition
+choices = ["rock", "paper", "scissors"]
+totalLives = 3
+playerLives = totalLives
+computerLives = totalLives
+player = False
